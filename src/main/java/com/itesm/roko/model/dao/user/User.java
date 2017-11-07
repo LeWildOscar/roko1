c9cf7f8c21db7ff2fcf850d33ac1c4961d16e876
alexCortizoFranza <alejandro.cortizo@udc.es>
2017-11-07 01:45:27 -0600

    Pruebas de cloud hibernate 2) - Cambios en la class user, agregados valores de ID y de Column

--- src/main/java/com/itesm/roko/model/dao/user/User.java
+++ src/main/java/com/itesm/roko/model/dao/user/User.java
@@ -22,170 +22,40 @@ import javax.persistence.Version;
 
 @Entity
 public class User {
+
+    @Id
     private int id;
+    @Column(name = "uuid")
     private String uuid;
+    @Column(name = "username")
     private String username;
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
     private String password;
+    @Column(name = "on_create")
     private Date onCreate;
+    @Column(name = "on_update")
     private Date onUpdate;
+    @Column(name = "total_points")
     private int total_points;
+    @Column(name = "losts")
     private int losts;
+    @Column(name = "played")
     private int played;
+    @Column(name = "country")
     private String country;
+    @Column(name = "total_money")
     private int total_money;
+    @Column(name = "money_in")
     private int money_in;
+    @Column(name = "money_out")
     private int money_out;
+    @Column(name = "wins")
     private int wins;
 
 
+    public User () {}
 
-    public int getId() {
 
-        return id;
-    }
-
-    public void setId(int id) {
-
-        this.id = id;
-    }
-
-    public String getUuid() {
-
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-
-        this.username = username;
-    }
-
-    public String getEmail() {
-
-        return email;
-    }
-
-    public void setEmail(String email) {
-
-        this.email = email;
-    }
-
-    public String getPassword() {
-
-        return password;
-    }
-
-    public void setPassword(String password) {
-
-        this.password = password;
-    }
-
-    public Date getOnCreate() {
-
-        return onCreate;
-    }
-
-    public void setOnCreate(Date onCreate) {
-
-        this.onCreate = onCreate;
-    }
-
-    public Date getOnUpdate() {
-
-        return onUpdate;
-    }
-
-    public void setOnUpdate(Date onUpdate) {
-
-        this.onUpdate = onUpdate;
-    }
-
-    public int getTotal_points() {
-
-        return total_points;
-    }
-
-    public void setTotal_points(int total_points) {
-
-        this.total_points = total_points;
-    }
-
-    public int getLosts() {
-
-        return losts;
-    }
-
-    public void setLosts(int losts) {
-
-        this.losts = losts;
-    }
-
-    public int getPlayed() {
-
-        return played;
-    }
-
-    public void setPlayed(int played) {
-
-        this.played = played;
-
-    }
-
-    public String getCountry() {
-
-        return country;
-    }
-
-    public void setCountry(String country) {
-
-        this.country = country;
-    }
-
-    public int getTotal_money() {
-
-        return total_money;
-    }
-
-    public void setTotal_money(int total_money) {
-
-        this.total_money = total_money;
-    }
-
-    public int getMoney_in() {
-
-        return money_in;
-    }
-
-    public void setMoney_in(int money_in) {
-
-        this.money_in = money_in;
-    }
-
-    public int getMoney_out() {
-
-        return money_out;
-    }
-
-    public void setMoney_out(int money_out) {
-
-        this.money_out = money_out;
-    }
-
-    public int getWins() {
-
-        return wins;
-    }
-
-    public void setWins(int wins) {
-
-        this.wins = wins;
-    }
 }
